25342fbc7edd4d34499a8b68cbad0f0870f5c580
claycode04 <yassdev4@gmail.com>
2025-07-27 19:14:36 +0100

    Refactor URL patterns to improve import organization and clarity

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
+from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -7,7 +8,7 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
