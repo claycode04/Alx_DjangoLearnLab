62254e92c5c7c01b5c8c01a740ede88f9b4c549f
claycode04 <yassdev4@gmail.com>
2025-08-09 15:33:07 +0100

    Add missing import statements for permissions and filters in views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,7 @@
 
 from rest_framework import generics, permissions, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework as django_filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
