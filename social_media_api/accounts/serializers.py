a12d9f999a480177c1d7abc6a5e26407767f762b
claycode04 <yassdev4@gmail.com>
2025-08-27 18:00:17 +0100

    Add token field to RegisterSerializer and update user creation logic

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,16 +1,20 @@
+
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    token = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2', 'bio', 'profile_picture')
+        fields = ('username', 'email', 'password', 'password2', 'bio', 'profile_picture', 'token')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -19,13 +23,15 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
+        token, created = Token.objects.get_or_create(user=user)
+        user.token = token.key
         return user
 
 class UserSerializer(serializers.ModelSerializer):
